ea5223ebb431caa7665854382eca89a23cd30b4b
xabigarrido <xabigarrido@gmail.com>
2025-01-30 20:54:48 +0100

    cambie un titul

--- src/tasks.js
+++ src/tasks.js
@@ -1,5 +1,5 @@
 export const task = [
   { id: 0, title: "Primera titulo", descripton: "some descripton" },
   { id: 1, title: "Segundo titulo", descripton: "some descripton" },
-  { id: 2, title: "some titulo", descripton: "some descripton" },
+  { id: 2, title: "Tercer titulo", descripton: "some descripton" },
 ];
